b074f83529a4a3bcdd3a713f99d3b6e1429cf123
andrew101sanders <andrew101sanders@gmail.com>
2024-04-05 06:11:13 -0400

    a

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-int calculateSum(int a, int b) {
-    return a + b;
-}
-
 int main()
 {
     int a, b;
     scanf("%d", &a);
     scanf("%d", &b);
-    int n = calculateSum(a, b);
+    int n = a + b;
     printf("The sum is %d\n", n);
 }
