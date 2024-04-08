ddb18ebb872f8b71edc9ffa52809f0afb2a1d02b
Andrew Sanders <andrew101sanders@gmail.com>
2024-04-07 21:53:19 -0400

    e

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -3,29 +3,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+void processUser() {
     char userInput[100];
     printf("Enter a username: ");
     fgets(userInput, sizeof(userInput), stdin);
     userInput[strcspn(userInput, "\n")] = '\0';  // Remove trailing newline
-    char command[100];
-    sprintf(command, "echo 'Processing user: %s'", input);  // CWE-78: Improper Neutralization of Special Elements used in an OS Command ('OS Command Injection')
-    system(command);
 
+    char command[100];
+    sprintf(command, "echo 'Processing user: %s'", userInput);  // CWE-78: Improper Neutralization of Special Elements used in an OS Command ('OS Command Injection')
+    system(command);
+}
+
+void processQuery() {
     char query[100];
     printf("Enter a query: ");
     fgets(query, sizeof(query), stdin);
     query[strcspn(query, "\n")] = '\0';  // Remove trailing newline
+
     char sql[200];
     sprintf(sql, "SELECT * FROM users WHERE username = '%s'", query);  // CWE-89: Improper Neutralization of Special Elements used in an SQL Command ('SQL Injection')
     printf("Executing SQL: %s\n", sql);
+}
 
+void processNumber() {
     int number;
     printf("Enter a number: ");
     scanf("%d", &number);
+
     int array[10];
     array[number] = 42;  // CWE-120: Buffer Overflow
     printf("Number processed: %d\n", number);
+}
+
+int main() {
+    processUser();
+    processQuery();
+    processNumber();
 
     return 0;
 }
