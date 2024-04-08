40b05e2b06b256251c087724005b82ce8fd0da35
Andrew Sanders <andrew101sanders@gmail.com>
2024-04-07 21:46:23 -0400

    mod

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -3,41 +3,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void processUser(char* input) {
-    char command[100];
-    sprintf(command, "echo 'Processing user: %s'", input);  // CWE-78: Improper Neutralization of Special Elements used in an OS Command ('OS Command Injection')
-    system(command);
-}
-
-void processQuery(char* query) {
-    char sql[200];
-    sprintf(sql, "SELECT * FROM users WHERE username = '%s'", query);  // CWE-89: Improper Neutralization of Special Elements used in an SQL Command ('SQL Injection')
-    printf("Executing SQL: %s\n", sql);
-}
-
-void processNumber(int number) {
-    int array[10];
-    array[number] = 42;  // CWE-120: Buffer Overflow
-    printf("Number processed: %d\n", number);
-}
-
 int main() {
     char userInput[100];
     printf("Enter a username: ");
     fgets(userInput, sizeof(userInput), stdin);
     userInput[strcspn(userInput, "\n")] = '\0';  // Remove trailing newline
-    processUser(userInput);
+    char command[100];
+    sprintf(command, "echo 'Processing user: %s'", input);  // CWE-78: Improper Neutralization of Special Elements used in an OS Command ('OS Command Injection')
+    system(command);
 
     char query[100];
     printf("Enter a query: ");
     fgets(query, sizeof(query), stdin);
     query[strcspn(query, "\n")] = '\0';  // Remove trailing newline
-    processQuery(query);
+    char sql[200];
+    sprintf(sql, "SELECT * FROM users WHERE username = '%s'", query);  // CWE-89: Improper Neutralization of Special Elements used in an SQL Command ('SQL Injection')
+    printf("Executing SQL: %s\n", sql);
 
     int number;
     printf("Enter a number: ");
     scanf("%d", &number);
-    processNumber(number);
+    int array[10];
+    array[number] = 42;  // CWE-120: Buffer Overflow
+    printf("Number processed: %d\n", number);
 
     return 0;
 }
